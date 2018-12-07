7c8342496b6649268b54528bad9845fd2efa86f0
Thiago Andrade <44100410+thiagoandrade14@users.noreply.github.com>
2018-12-07 12:01:14 -0500

    Stable version that beats team socket on 1v1 (failbot), 4v2 and 4v4

--- Student Robot/edu/ufl/cise/cs1/robots/Pristis.java
+++ Student Robot/edu/ufl/cise/cs1/robots/Pristis.java
@@ -1,5 +1,6 @@
 package edu.ufl.cise.cs1.robots;
 
+import robocode.HitRobotEvent;
 import robocode.ScannedRobotEvent;
 import robocode.TeamRobot;
 import robocode.util.Utils;
@@ -10,13 +11,13 @@ import robocode.util.Utils;
  * This robot uses a very simple implementation of the stop-and-go strategy
  * to dodge bullets.
  *
- * The targeting algorithm uses linear targeting (based on trigonometry)
+ * The targeting algorithm uses linear targeting based on trigonometry
  * in order to "predict" the position of target when the bullet has
  * travelled long enough to reach it. It is not optimized, but works much
  * better than just shooting straight.
  *
- * All strategies used are found in the robocode wiki and completely implemented
- * by the author, except when noted.
+ * All ideas and strategies used were found in sample robots or in the robocode wiki.
+ * They were completely implemented by the author, except when noted.
  *
  */
 
@@ -24,43 +25,66 @@ public class Pristis extends TeamRobot {
 
     private double previousTargetEnergy = 100; //keeps track of target's energy
     private boolean lastMove = false; //used to reverse direction
+    private String enemyOfTheState = "nemesis";
+    private boolean wasRammed = false;
 
     public void run() {
-        //FIXME: program to go close to center to patrol, if target isn't found
-
-        setAdjustGunForRobotTurn(true);
+        setAdjustGunForRobotTurn(true); //Adjusts Gun and Radar to Robot's body turn
         setAdjustRadarForGunTurn(true);
-        //Adjusts Gun and Radar to Robot's body turn
-
         while (true) {
-            turnRadarLeft(Double.POSITIVE_INFINITY);
+            getOutOfEdge();
+            turnRadarLeftRadians(Double.POSITIVE_INFINITY);
             //constantly rotates radar and patrols until finds target
         }
     }
 
     public void onScannedRobot(ScannedRobotEvent target) {
-        //FIXME program to approach very distant targets
         if (isTeammate(target.getName())) {
             return; //if target is a teammate, return to run() behavior and look for next target
         }
-        double absBearing = getHeadingRadians() + target.getBearingRadians();
-        setTurnRightRadians(Utils.normalRelativeAngle(target.getBearingRadians() - Math.PI/2));
-        //keeps a 90 degree angle relative to the target, making dodging more efficient
         double firePower = fireManagement(target);
-        double gunTurnAngle;
-        double radarTurnAngle;
-        double predictedAngleDifference = targetSystem(target, absBearing);
-        gunTurnAngle = absBearing - getGunHeadingRadians() + predictedAngleDifference;
-        radarTurnAngle = absBearing - getRadarHeadingRadians();
-        setTurnRadarRightRadians(Utils.normalRelativeAngle(radarTurnAngle));
-        setTurnGunRightRadians(Utils.normalRelativeAngle(gunTurnAngle));
-        //locking gun on target
-        fire(firePower);
-        stopAndGo(target); //move if enemy has shot last turn
+        double absBearing = getHeadingRadians() + target.getBearingRadians();
+        enemyOfTheState = target.getName();
+
+        if (target.getEnergy() < 20) { //target is weak
+            if (getEnergy() > 30) { //recklessly charge
+                targetSystem(target, absBearing, firePower);
+                fire(fireManagement(target));
+                charge(absBearing);
+            }
+            else {
+                if (target.getDistance() > 100) { //approach weak enemy to finish off with bullets
+                    charge(absBearing);
+                }
+                targetSystem(target, absBearing, firePower);
+                fire(fireManagement(target));
+            }
+        }
+        else {
+            setTurnRightRadians(Utils.normalRelativeAngle(target.getBearingRadians() - Math.PI / 2));
+            //keeps a 90 degree angle relative to the target, making dodging more efficient
+            targetSystem(target, absBearing, firePower);
+            fire(firePower);
+            stopAndGo(target); //move if enemy has shot last turn
+        }
+    }
+
+    public void onHitRobot(HitRobotEvent event) {
+        if (!event.getName().equals(enemyOfTheState) && !event.isMyFault() && !wasRammed) {
+            if (event.getBearingRadians() < Math.PI/2 || event.getBearingRadians() > 3* Math.PI/2) {
+                setBack(100);
+            }
+            else {
+                setAhead(100);
+            }
+            out.println("Ouch!");
+            turnRadarLeftRadians(event.getBearingRadians() + getRadarHeadingRadians());
+            enemyOfTheState = event.getName();
+            wasRammed = true;
+        }
     }
 
     private void stopAndGo(ScannedRobotEvent target) {
-        //FIXME: program to avoid walls while stopping and going.
         //stop and go strategy: if target's energy depleted by less than 3
         // (meaning it fired last turn), move ahead or back
         if (previousTargetEnergy <= target.getEnergy() + 3 && previousTargetEnergy > target.getEnergy()) {
@@ -76,18 +100,24 @@ public class Pristis extends TeamRobot {
         previousTargetEnergy = target.getEnergy();
     }
 
-    private double targetSystem(ScannedRobotEvent target, double absBearing) {
-        return (target.getVelocity() * Math.sin(target.getHeadingRadians() - absBearing) / 13.0); //"predicts" future position
-        // based on target current velocity and bearing. FIXME ADD REFERENCE and explanation
+    private void targetSystem(ScannedRobotEvent target, double absBearing, double firePower) {
+        /*"predicts" future position based on target current velocity
+         and bearing. Code was implemented by the author, but inspired by tutorial and formulas found at
+         robowiki.net/wiki/Linear_targeting
+        */
+        double predictedChange = (target.getVelocity() * Math.sin(target.getHeadingRadians()
+                                                       - absBearing) / (20 - (3*firePower)));
+        double gunTurnAngle = absBearing - getGunHeadingRadians() + predictedChange;
+        double radarTurnAngle = absBearing - getRadarHeadingRadians();
+        setTurnRadarRightRadians(Utils.normalRelativeAngle(radarTurnAngle));
+        setTurnGunRightRadians(Utils.normalRelativeAngle(gunTurnAngle));
     }
 
     private double fireManagement(ScannedRobotEvent target) {
+        //This method manages the fire power
         double firePower;
-        if (target.getEnergy() < 3.0) {
-            firePower = target.getEnergy() + 0.1; //avoids wasting energy on dying or disabled targets.
-        }
-        //choose firePower based on distance to target
-        else if (target.getDistance() < 175) {
+        //Else, choose firePower based on distance to target
+        if (target.getDistance() < 175) {
             firePower = 3;
         }
         else if(target.getDistance() < 300) {
@@ -96,6 +126,40 @@ public class Pristis extends TeamRobot {
         else {
             firePower = 1;
         }
+        //If energy level drops too low, save power...
+        if (getEnergy() < 20) {
+            firePower = 0.1;
+        }
+        if (target.getEnergy() < 3.0) { //...except to get that greedy kill
+            firePower = target.getEnergy() + 0.1;
+        }
         return firePower;
+    }
+
+    private void charge(double absBearing) {
+        //recklessly charge at low energy target
+        setTurnRightRadians(absBearing - getHeadingRadians());
+        setAhead(100);
+    }
+
+    private void getOutOfEdge() {
+        //Moves away from edges, where radar range could possibly
+        //not scan enemies too far away
+        if (getX() < 150) {
+            turnLeftRadians(Math.PI/2);
+            setAhead(100);
+        }
+        else if (getX() > getBattleFieldWidth() - 150) {
+            turnLeftRadians(3 * Math.PI/2);
+            setAhead(100);
+        }
+        if (getY() < 150) {
+            turnLeftRadians(0);
+            setAhead(100);
+        }
+        else if (getY() > getBattleFieldHeight() - 150) {
+            turnLeftRadians(Math.PI);
+            setAhead(100);
+        }
     }
 }
